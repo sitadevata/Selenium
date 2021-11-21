a69ba0b813a13f40ac22e6397105b2f772c317cd
sitadevata <sitadevata@gmail.com>
2021-11-21 11:37:41 +0530

    Reformatted code

--- src/SpreeTesting/FindingElements.java
+++ src/SpreeTesting/FindingElements.java
@@ -9,29 +9,24 @@ import org.openqa.selenium.support.ui.Select;
 import java.security.spec.NamedParameterSpec;
 
 public class FindingElements {
-
     public static void main(String args[]) {
         System.setProperty("webdriver.chrome.driver", "src/SpreeTesting/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.get("https://spree-vapasi-prod.herokuapp.com/login");
-         driver.findElement(By.id("spree_user_email")).sendKeys("sita2.devata@test.com");
-         driver.findElement(By.name("spree_user[password]")).sendKeys( "Daddy@12345678");
+        driver.findElement(By.id("spree_user_email")).sendKeys("sita2.devata@test.com");
+        driver.findElement(By.name("spree_user[password]")).sendKeys( "Daddy@12345678");
         // driver.findElement(By.cssSelector("input[name=\"spree_user[password_confirmation]\"]")).sendKeys( "Daddy@12345678");
-       driver.findElement(By.cssSelector("input[type=\"checkbox\"]")).click();
+        driver.findElement(By.cssSelector("input[type=\"checkbox\"]")).click();
         driver.findElement(By.name("commit")).click();
 
         WebElement dropdown = driver.findElement(By.id("taxon"));
         Select Select = new Select(dropdown);
-                Select.selectByValue("1");
+        Select.selectByValue("1");
 
-               // driver.findElement(By.cssSelector("span[title='Ruby.on.Rails.Bag']")).click();
-                //driver.findElement(By.name("quantity")).sendKeys("1");
-
-
-
-                //driver.findElement(By.xpath("[@id="keywords"]"));
-         driver.findElement(By.cssSelector("input.btn.btn-lg.btn-success.btn-block")).click();
-
-        //driver.findElement(By.cssSelector(["spree_user[password_confirmation"])).sendKeys(..CharSequences:"Daddy@12345678");
+        //driver.findElement(By.cssSelector("span[title='Ruby.on.Rails.Bag']")).click();
+        //driver.findElement(By.name("quantity")).sendKeys("1");
+        //driver.findElement(By.xpath("[@id="keywords"]"));
+        driver.findElement(By.cssSelector("input.btn.btn-lg.btn-success.btn-block")).click();
+        //driver.findElement(By.cssSelector(["spree_user[password_confirmation"])).sendKeys(..CharSequences:"Daddy@");
     }
 }
--- src/SpreeTesting/OpeningBroswer.java
+++ src/SpreeTesting/OpeningBroswer.java
@@ -6,19 +6,17 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OpeningBroswer {
-
     public static void main(String args[]){
         System.setProperty("webdriver.chrome.driver","src/SpreeTesting/chromedriver");
         WebDriver driver =new ChromeDriver();
         driver.get("https://spree-vapasi-prod.herokuapp.com/");
-       // WebElement logoElement = driver.findElement(By.id("logo"));
-       // logoElement.click();
-       // WebElement dropbox = driver.findElement(By.id("taxon"));
-        //dropbox.click();
-        //WebElement searchBox = driver.findElement(By.id("keywords"));
-        //searchBox.click();
+        WebElement logoElement = driver.findElement(By.id("logo"));
+        logoElement.click();
+        WebElement dropbox = driver.findElement(By.id("taxon"));
+        dropbox.click();
+        WebElement searchBox = driver.findElement(By.id("keywords"));
+        searchBox.click();
         WebElement Login = driver.findElement(By.xpath("//*[@id=\"home-page\"]"));
-       // Login.click();
-        //searchBox.
+        Login.click();
     }
 }
